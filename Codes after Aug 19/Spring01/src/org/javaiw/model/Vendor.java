236c76276b2514d9109215dcaf3915522baa6836
Sanket Jain <sj2659@columbia.edu>
2016-08-24 13:19:04 -0400

    Spring and xml
    Code of spring with xml

---
+++ Codes after Aug 19/Spring01/src/org/javaiw/model/Vendor.java
@@ -0,0 +1,38 @@
+package org.javaiw.model;
+
+public class Vendor {
+	
+	String vemail, vname;
+
+	public Vendor(String vemail, String vname) {
+		super();
+		this.vemail = vemail;
+		this.vname = vname;
+	}
+
+	public Vendor() {
+		super();
+	}
+
+	public String getVemail() {
+		return vemail;
+	}
+	public void setVemail(String vemail) {
+		this.vemail = vemail;
+	}
+	public String getVname() {
+		return vname;
+	}
+	public void setVname(String vname) {
+		this.vname = vname;
+	}
+
+	@Override
+	public String toString() {
+		// TODO Auto-generated method stub
+		return vname + " = " + vemail;
+	}
+	
+	
+
+}
